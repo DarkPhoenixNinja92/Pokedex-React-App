c59362694659b4032f2271b2ee3dbbd5d15574cc
Shaun <scpour@gmail.com>
2023-12-08 14:35:08 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react'
+import Navbar from './components/layout/Navbar'
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+    <div>
+        <h1>Shaun Pour</h1>
+    </div>
+    )
+  }
+}
